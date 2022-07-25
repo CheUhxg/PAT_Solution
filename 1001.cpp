0108e21c5112465e456949f7b972e196c11db6b0
cheuhxg <3193079152@qq.com>
2022-07-25 17:08:14 +0800

    add 1001

---
+++ 1001.cpp
@@ -0,0 +1,21 @@
+#include<algorithm>
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+//int main() {
+//  int a, b, cnt = 0; cin >> a >> b;
+//  a += b;
+//
+//  string ret;
+//  while (a >= 10 || a <= -10) {
+//    ret.push_back(abs(a % 10) + '0');
+//    a /= 10;
+//    if (++cnt % 3 == 0)
+//      ret.push_back(',');
+//  }
+//  cout << a;
+//  reverse(ret.begin(), ret.end());
+//  cout << ret << endl;
+//}
