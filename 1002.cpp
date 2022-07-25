bbe12503ed0c4821ed49428d9b6727c813f9654b
cheuhxg <3193079152@qq.com>
2022-07-25 17:08:22 +0800

    add 1002

---
+++ 1002.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+float map[1001];
+//int main() {
+//  int k, a, cnt;
+//  double b;
+//  cin >> k;
+//  cnt = k;
+//  for (int i = 0; i < k; ++i) {
+//    cin >> a >> b;
+//    map[a] = b;
+//  }
+//  cin >> k;
+//  for (int i = 0; i < k; ++i) {
+//    cin >> a >> b;
+//    if (abs(map[a] - 0) < 0.01)++cnt;
+//    map[a] += b;
+//    if (abs(map[a] - 0) < 0.01)--cnt;
+//  }
+//  cout << cnt;
+//  for (int i = 1000; i >= 0; --i) {
+//    if (abs(map[i] - 0) > 0.01)
+//      printf(" %d %.1f", i, map[i]);
+//  }
+//  cout << endl;
+//}
