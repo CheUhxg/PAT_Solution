2b56aef803b4f61315707b0a952e755822ac34c4
cheuhxg <3193079152@qq.com>
2022-07-26 22:05:20 +0800

    mute 1011

--- 1011.cpp
+++ 1011.cpp
@@ -4,21 +4,21 @@
 
 using namespace std;
 
-int main() {
-	const char m[4] = "WTL";
-	double profit = 0.65, tmp;
-	for (int i = 0; i < 3; ++i) {
-		double max_game = 0;
-		int res = 0;
-		for (int j = 0; j < 3; ++j) {
-			cin >> tmp;
-			if (tmp > max_game) {
-				max_game = tmp;
-				res = j;
-			}
-		}
-		cout << m[res] << ' ';
-		profit *= max_game;
-	}
-	printf("%.2f\n", (profit - 1) * 2);
-}
+//int main() {
+//	const char m[4] = "WTL";
+//	double profit = 0.65, tmp;
+//	for (int i = 0; i < 3; ++i) {
+//		double max_game = 0;
+//		int res = 0;
+//		for (int j = 0; j < 3; ++j) {
+//			cin >> tmp;
+//			if (tmp > max_game) {
+//				max_game = tmp;
+//				res = j;
+//			}
+//		}
+//		cout << m[res] << ' ';
+//		profit *= max_game;
+//	}
+//	printf("%.2f\n", (profit - 1) * 2);
+//}
