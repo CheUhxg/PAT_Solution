69916476ccc3a0e44a9a981a90502f6dcf6b90a3
cheuhxg <3193079152@qq.com>
2022-07-29 20:35:09 +0800

    test point 5 failed

--- 1014.cpp
+++ 1014.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <stdio.h>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-bool cmp(const vector<int>& t1, const vector<int>& t2) {
-	return t1[0] < t2[0];
-}
-
 int main() {
 	int n, m, k, q;	cin >> n >> m >> k >> q;
-	vector<vector<int>>take(k, vector<int>(2));
-	vector<int> finish_time(k), end_time(n);
+	vector<int>take(k), finish_time(k);
+	vector<queue<int>> queues(n, queue<int>());
+
 	for (int i = 0; i < k; ++i) {
-		cin >> take[i][0];
-		take[i][1] = i;
-	}
-	sort(take.begin(), take.end(), cmp);
-	for (int i = 0; i < k; ++i) {
+		cin >> take[i];
 		if (i < n * m) {
-			if (i < n)finish_time[take[i][1]] = take[i][0];
-			else finish_time[take[i][1]] = take[i][0] + end_time[i % n];
-			end_time[i % n] = finish_time[take[i][1]];
+			if (i < n)finish_time[i] = take[i];
+			else finish_time[i] = take[i] + finish_time[i - n];
+			queues[i % n].push(i);
 		}
 		else {
 			int index = 0;
 			for (int j = 1; j < n; ++j) {
-				if (end_time[index] > end_time[j]) {
+				if (take[queues[j].front()] < take[queues[index].front()]) {
 					index = j;
 				}
 			}
-			end_time[index] += take[i][0];
-			finish_time[i] = end_time[index];
+			finish_time[i] = take[i] + finish_time[queues[index].back()];
+			queues[index].push(i);
+			queues[index].pop();
 		}
 	}
 	for (int i = 0; i < q; ++i) {
 		cin >> k;
 		--k;
-		n = finish_time[k] / 60;
-		m = finish_time[k] % 60;
-		if (n >= 9 && m > 0) {
+		if (finish_time[k] - take[k] >= 540)
 			cout << "Sorry" << endl;
-		}
-		else {
-			printf("%02d:%02d\n", n + 8, m);
-		}
+		else
+			printf("%02d:%02d\n", finish_time[k] / 60 + 8, finish_time[k] % 60);
 	}
 }
