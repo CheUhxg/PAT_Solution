8a2d8dceb4626b0fe31aff59a30f4220be4feb23
cheuhxg <3193079152@qq.com>
2022-07-30 16:36:02 +0800

    finish 1015

---
+++ 1015.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int parse(int n, const int d) {
+	int res = 0;
+
+	while (n > 0) {
+		res *= d;
+		res += n % d;
+		n /= d;
+	}
+
+	return res;
+}
+
+bool isPrime(const int d) {
+	if (d <= 1)return false;
+	for (int i = 2; i * i <= d; ++i) {
+		if (d % i == 0)
+			return false;
+	}
+	return true;
+}
+
+//int main() {
+//	int n, d;
+//	while (cin >> n && n > 0) {
+//		cin >> d;
+//		if (!isPrime(n)) {
+//			cout << "No" << endl;
+//			continue;
+//		}
+//		n = parse(n, d);
+//		cout << (isPrime(n) ? "Yes" : "No") << endl;
+//	}
+//}
