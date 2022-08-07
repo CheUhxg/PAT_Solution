97e3fe81d1c81124228a7a3293a41b0766aa527a
cheuhxg <3193079152@qq.com>
2022-08-07 09:01:35 +0800

    mute

--- 1018.cpp
+++ 1018.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-vector<int> res, bikes;
-int c, n, sp, m, a, b, min_dis = MAX_DIS, min_send = MAX_DIS, min_back = MAX_DIS;
+static vector<int> res, bikes;
+static int c, n, sp, m, a, b, min_dis = MAX_DIS, min_send = MAX_DIS, min_back = MAX_DIS;
 
-void dfs(const vector<vector<int>>& g, const int now, const int dis, vector<int>& path) {
+static void dfs(const vector<vector<int>>& g, const int now, const int dis, vector<int>& path) {
 	if (now == sp) {
 		int send = 0, back = 0;
 		for (const int node : path) {
@@ -58,27 +58,27 @@ void dfs(const vector<vector<int>>& g, const int now, const int dis, vector<int>
 	}
 }
 
-int main() {
-	cin >> c >> n >> sp >> m;
-	bikes.resize(n + 1);
-	vector<bool>visited(n + 1, false);
-	vector<vector<int>> g(n + 1, vector<int>(n + 1, MAX_DIS));
-	bikes[0] = 0;
-	visited[0] = true;
-	for (int i = 1; i <= n; ++i) {
-		cin >> bikes[i];
-	}
-	for (int i = 1; i <= m; ++i) {
-		cin >> a >> b;
-		cin >> g[a][b];
-		g[b][a] = g[a][b];
-	}
-	vector<int> path;
-	dfs(g, 0, 0, path);
-
-	cout << (min_send < 0 ? 0 : min_send) << ' ' << 0;
-	for (const int s : res) {
-		cout << "->" << s;
-	}
-	cout << ' ' << min_back << endl;
-}
+//int main() {
+//	cin >> c >> n >> sp >> m;
+//	bikes.resize(n + 1);
+//	vector<bool>visited(n + 1, false);
+//	vector<vector<int>> g(n + 1, vector<int>(n + 1, MAX_DIS));
+//	bikes[0] = 0;
+//	visited[0] = true;
+//	for (int i = 1; i <= n; ++i) {
+//		cin >> bikes[i];
+//	}
+//	for (int i = 1; i <= m; ++i) {
+//		cin >> a >> b;
+//		cin >> g[a][b];
+//		g[b][a] = g[a][b];
+//	}
+//	vector<int> path;
+//	dfs(g, 0, 0, path);
+//
+//	cout << (min_send < 0 ? 0 : min_send) << ' ' << 0;
+//	for (const int s : res) {
+//		cout << "->" << s;
+//	}
+//	cout << ' ' << min_back << endl;
+//}
