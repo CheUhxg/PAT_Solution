03c98387a37a4c6cad7d2a0e9affb6fb6609248f
cheuhxg <3193079152@qq.com>
2022-08-08 19:13:37 +0800

    mute

--- 1019.cpp
+++ 1019.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool isPalin(const vector<int>& str) {
+static bool isPalin(const vector<int>& str) {
 	for (int i = 0; 2 * i < str.size(); ++i) {
 		if (str[i] != str[str.size() - i - 1]) {
 			return false;
@@ -12,16 +12,16 @@ bool isPalin(const vector<int>& str) {
 	return true;
 }
 
-int main() {
-	int n, base; cin >> n >> base;
-	vector<int> str;
-	while (n > 0) {
-		str.push_back(n % base);
-		n /= base;
-	}
-	cout << (isPalin(str) ? "Yes" : "No") << endl;
-	cout << str.back();
-	for (int i = str.size() - 2; i >= 0; --i) {
-		cout << ' ' << str[i];
-	}
-}
+//int main() {
+//	int n, base; cin >> n >> base;
+//	vector<int> str;
+//	while (n > 0) {
+//		str.push_back(n % base);
+//		n /= base;
+//	}
+//	cout << (isPalin(str) ? "Yes" : "No") << endl;
+//	cout << str.back();
+//	for (int i = str.size() - 2; i >= 0; --i) {
+//		cout << ' ' << str[i];
+//	}
+//}
