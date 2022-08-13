d00b1e517d07d19252e74230ee274322415a2fa4
cheuhxg <3193079152@qq.com>
2022-08-13 10:16:13 +0800

    finish 1023

---
+++ 1023.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int add = 0, n, table[10] = { 0 };
+	string num;
+	getline(cin, num);
+	for (int i = num.size() - 1; i >= 0; --i) {
+		++table[num[i] - '0'];
+		n = (num[i] - '0') * 2 + add;
+		num[i] = n % 10 + '0';
+		add = n / 10;
+		--table[num[i] - '0'];
+	}
+	if (add == 0) {
+		for (const int d : table) {
+			if (d) {
+				add = 1;
+				break;
+			}
+		}
+		if (add == 0) {
+			cout << "Yes" << endl
+				<< num << endl;
+			return 0;
+		}
+		add = 0;
+	}
+	cout << "No" << endl;
+	if(add == 1)cout << add;
+	cout << num << endl;
+}
