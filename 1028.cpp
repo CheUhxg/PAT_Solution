e712b35e38fc380a4d58b3439ba1ed9f3848e741
cheuhxg <3193079152@qq.com>
2022-08-18 10:48:40 +0800

    finish 1028

--- 1028.cpp
+++ 1028.cpp
@@ -1,10 +1,49 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<vector>
 #include<cstdio>
 
 using namespace std;
 
-int main() {
+struct Student {
+	short grade;
+	string id, name;
+};
 
+bool cmp1(const Student& s1, const Student& s2) {
+	return s1.id < s2.id;
+}
+
+bool cmp2(const Student& s1, const Student& s2) {
+	if(s1.name == s2.name)return s1.id < s2.id;
+	return s1.name < s2.name;
+}
+
+bool cmp3(const Student& s1, const Student& s2) {
+	if (s1.grade == s2.grade)return s1.id < s2.id;
+	return s1.grade < s2.grade;
+}
+
+int main() {
+	int n, c; cin >> n >> c; cin.get();
+	vector<Student> student(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> student[i].id >> student[i].name >> student[i].grade;
+		cin.get();
+	}
+	switch (c) {
+	case 1:
+		sort(student.begin(), student.end(), cmp1);
+		break;
+	case 2:
+		sort(student.begin(), student.end(), cmp2);
+		break;
+	case 3:
+		sort(student.begin(), student.end(), cmp3);
+		break;
+	}
+	for (const Student& s : student) {
+		cout << s.id << ' ' << s.name << ' ' << s.grade << endl;
+	}
 }
