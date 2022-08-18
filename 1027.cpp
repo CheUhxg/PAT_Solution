08ad58f68d4d8214130823bc2e03a792b2b48281
cheuhxg <3193079152@qq.com>
2022-08-18 10:33:43 +0800

    mute

--- 1027.cpp
+++ 1027.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-char convert(int num) {
+static char convert(int num) {
 	if (num < 10) {
 		return num + '0';
 	}
@@ -14,17 +14,17 @@ char convert(int num) {
 	}
 }
 
-int main() {
-	int rgb[3];
-	string res;
-	cin >> rgb[0] >> rgb[1] >> rgb[2];
-	for (int i = 2; i >= 0; --i) {
-		for (int j = 0; j < 2; ++j) {
-			res.push_back(convert(rgb[i] % 13));
-			rgb[i] /= 13;
-		}
-	}
-	res.push_back('#');
-	reverse(res.begin(), res.end());
-	cout << res << endl;
-}
+//int main() {
+//	int rgb[3];
+//	string res;
+//	cin >> rgb[0] >> rgb[1] >> rgb[2];
+//	for (int i = 2; i >= 0; --i) {
+//		for (int j = 0; j < 2; ++j) {
+//			res.push_back(convert(rgb[i] % 13));
+//			rgb[i] /= 13;
+//		}
+//	}
+//	res.push_back('#');
+//	reverse(res.begin(), res.end());
+//	cout << res << endl;
+//}
