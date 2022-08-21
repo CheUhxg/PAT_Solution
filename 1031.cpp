8e0d48acf4cb95c2b67147be2ed5ab102c7873fe
cheuhxg <3193079152@qq.com>
2022-08-21 09:53:44 +0800

    mute

--- 1031.cpp
+++ 1031.cpp
@@ -4,25 +4,25 @@
 
 using namespace std;
 
-int main() {
-	string str; getline(cin, str);
-	int length = str.size(), v = 0, h, tmp;
-	for (h = 3; h <= length; ++h) {
-		tmp = (length + 2 - h) / 2;
-		if (tmp * 2 == length + 2 - h && tmp <= h) {
-			v = max(v, tmp);
-		}
-	}
-	--v;
-	h = length - 2 * v;
-	for (tmp = 0; tmp < v; ++tmp) {
-		cout << str[tmp];
-		for (int j = 0; j < h - 2;++j) {
-			cout << ' ';
-		}
-		cout << str[length - 1 - tmp] << endl;
-	}
-	for (int i = 0; i < h; ++i) {
-		cout << str[tmp + i];
-	}
-}
+//int main() {
+//	string str; getline(cin, str);
+//	int length = str.size(), v = 0, h, tmp;
+//	for (h = 3; h <= length; ++h) {
+//		tmp = (length + 2 - h) / 2;
+//		if (tmp * 2 == length + 2 - h && tmp <= h) {
+//			v = max(v, tmp);
+//		}
+//	}
+//	--v;
+//	h = length - 2 * v;
+//	for (tmp = 0; tmp < v; ++tmp) {
+//		cout << str[tmp];
+//		for (int j = 0; j < h - 2;++j) {
+//			cout << ' ';
+//		}
+//		cout << str[length - 1 - tmp] << endl;
+//	}
+//	for (int i = 0; i < h; ++i) {
+//		cout << str[tmp + i];
+//	}
+//}
