f2536b12281aa10a6d6e751483c70b571267bbf0
cheuhxg <3193079152@qq.com>
2022-08-21 11:28:32 +0800

    finish 1032

--- 1032.cpp
+++ 1032.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
-#include <algorithm>
-#include <unordered_set>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
 	char c;
-	int a, b, n, meet = -1; cin >> a >> b >> n;
-	unordered_set<int>visited;
+	int sa, sb, a, b, n, meet; cin >> sa >> sb >> n;
+	unordered_map<int, int>next;
 	for (int i = 0; i < n; ++i) {
 		cin >> a >> c >> b;
-		if (visited.count(b) > 0) meet = b;
-		else visited.insert(b);
+		next[a] = b;
 	}
-	if (meet == -1) {
-		cout << meet << endl;
+	vector<int> v1, v2;
+	while (sa >= 0) {
+		v1.push_back(sa);
+		sa = next[sa];
 	}
-	else {
-		printf("%05d\n", meet);
+	while (sb >= 0) {
+		v2.push_back(sb);
+		sb = next[sb];
 	}
+	for (meet = 1; meet <= v1.size() && meet <= v2.size() && v1[v1.size() - meet] == v2[v2.size() - meet]; ++meet);
+	if (meet == 1)cout << -1 << endl;
+	else printf("%05d\n", v1[v1.size() - meet + 1]);
 }
