8cec9b72655780bd01b12c42ac95d28d3399f0e2
cheuhxg <3193079152@qq.com>
2022-08-21 09:06:27 +0800

    mute

--- 1030.cpp
+++ 1030.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-struct City
+static struct City
 {
 	int destin, cost, length;
 };
 
-vector<int> res;
-int n, m, s, d, min_length = INF, min_cost = INF;
+static vector<int> res;
+static int n, m, s, d, min_length = INF, min_cost = INF;
 
-void dfs(const int start, const int length, const int cost,
+static void dfs(const int start, const int length, const int cost,
 				const vector<vector<City>> g, vector<bool>& visited, vector<int>& path) {
 	path.push_back(start);
 	if (start == d) {
@@ -42,19 +42,19 @@ void dfs(const int start, const int length, const int cost,
 	path.pop_back();
 }
 
-int main() {
-	int c1, c2, length, cost, mid; cin >> n >> m >> s >> d;
-	vector<vector<City>> g(n);
-	for (int i = 0; i < m; ++i) {
-		cin >> c1 >> c2 >> length >> cost;
-		g[c1].push_back({ c2, cost, length });
-		g[c2].push_back({ c1, cost, length });
-	}
-	vector<int>path;
-	vector<bool>visited(n, false);
-	dfs(s, 0, 0, g, visited, path);
-	for (const int node : res) {
-		cout << node << ' ';
-	}
-	cout << min_length << ' ' << min_cost << endl;
-}
+//int main() {
+//	int c1, c2, length, cost, mid; cin >> n >> m >> s >> d;
+//	vector<vector<City>> g(n);
+//	for (int i = 0; i < m; ++i) {
+//		cin >> c1 >> c2 >> length >> cost;
+//		g[c1].push_back({ c2, cost, length });
+//		g[c2].push_back({ c1, cost, length });
+//	}
+//	vector<int>path;
+//	vector<bool>visited(n, false);
+//	dfs(s, 0, 0, g, visited, path);
+//	for (const int node : res) {
+//		cout << node << ' ';
+//	}
+//	cout << min_length << ' ' << min_cost << endl;
+//}
