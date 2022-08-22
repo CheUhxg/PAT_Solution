db69fe4b0deb4f47ebcb05c9f2242a9d5d1f740a
cheuhxg <3193079152@qq.com>
2022-08-22 09:36:01 +0800

    mute

--- 1032.cpp
+++ 1032.cpp
@@ -6,24 +6,24 @@
 
 using namespace std;
 
-int main() {
-	char c;
-	int sa, sb, a, b, n, meet; cin >> sa >> sb >> n;
-	unordered_map<int, int>next;
-	for (int i = 0; i < n; ++i) {
-		cin >> a >> c >> b;
-		next[a] = b;
-	}
-	vector<int> v1, v2;
-	while (sa >= 0) {
-		v1.push_back(sa);
-		sa = next[sa];
-	}
-	while (sb >= 0) {
-		v2.push_back(sb);
-		sb = next[sb];
-	}
-	for (meet = 1; meet <= v1.size() && meet <= v2.size() && v1[v1.size() - meet] == v2[v2.size() - meet]; ++meet);
-	if (meet == 1)cout << -1 << endl;
-	else printf("%05d\n", v1[v1.size() - meet + 1]);
-}
+//int main() {
+//	char c;
+//	int sa, sb, a, b, n, meet; cin >> sa >> sb >> n;
+//	unordered_map<int, int>next;
+//	for (int i = 0; i < n; ++i) {
+//		cin >> a >> c >> b;
+//		next[a] = b;
+//	}
+//	vector<int> v1, v2;
+//	while (sa >= 0) {
+//		v1.push_back(sa);
+//		sa = next[sa];
+//	}
+//	while (sb >= 0) {
+//		v2.push_back(sb);
+//		sb = next[sb];
+//	}
+//	for (meet = 1; meet <= v1.size() && meet <= v2.size() && v1[v1.size() - meet] == v2[v2.size() - meet]; ++meet);
+//	if (meet == 1)cout << -1 << endl;
+//	else printf("%05d\n", v1[v1.size() - meet + 1]);
+//}
