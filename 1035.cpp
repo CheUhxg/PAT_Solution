59172c22f0c6cb6418379e210fb42ed5a9b11f19
cheuhxg <3193079152@qq.com>
2022-08-23 13:20:22 +0800

    finish 1035

--- 1035.cpp
+++ 1035.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-int main() {
+struct Team {
+	string name, passwd;
+};
 
+int main() {
+	bool mod;
+	vector<Team> res;
+	string name, passwd;
+	int n; cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> name >> passwd;
+		mod = false;
+		for (int j = 0; j < passwd.size(); ++j) {
+			switch (passwd[j]) {
+			case '1':
+				mod = true;
+				passwd[j] = '@';
+				break;
+			case 'l':
+				mod = true;
+				passwd[j] = 'L';
+				break;
+			case '0':
+				mod = true;
+				passwd[j] = '%';
+				break;
+			case 'O':
+				mod = true;
+				passwd[j] = 'o';
+				break;
+			default:
+				break;
+			}
+		}
+		if (mod) res.push_back({ name, passwd });
+	}
+	if (res.empty()) {
+		if (n == 1)
+			cout << "There is 1 account and no account is modified" << endl;
+		else
+			cout << "There are " << n
+			<< " accounts and no account is modified" << endl;
+	}
+	else {
+		cout << res.size() << endl;
+		for (const Team& t : res) {
+			cout << t.name << ' ' << t.passwd << endl;
+		}
+	}
 }
