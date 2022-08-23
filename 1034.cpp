9a7301d171edd6a5651612b316d515ca5af5db43
cheuhxg <3193079152@qq.com>
2022-08-23 13:00:24 +0800

    mute

--- 1034.cpp
+++ 1034.cpp
@@ -6,12 +6,12 @@
 
 using namespace std;
 
-struct Ganster {
+static struct Ganster {
 	unordered_set<string> related;
 	int time;
 };
 
-void dfs(unordered_set<string>& visited, const string& now, map<string, Ganster>& g) {
+static void dfs(unordered_set<string>& visited, const string& now, map<string, Ganster>& g) {
 	visited.insert(now);
 	for (const auto& other : g[now].related) {
 		if (visited.count(other) == 0) {
@@ -20,46 +20,46 @@ void dfs(unordered_set<string>& visited, const string& now, map<string, Ganster>
 	}
 }
 
-int main() {
-	string name1, name2;
-	int n, k, t; cin >> n >> k;
-	map<string, Ganster> g;
-	map<string, int> res;
-	for (int i = 0; i < n; ++i) {
-		cin >> name1 >> name2 >> t;
-		g[name1].related.insert(name2);
-		g[name1].time += t;
-
-		g[name2].related.insert(name1);
-		g[name2].time += t;
-	}
-	t = 0;
-	unordered_set<string> visited;
-	for (const auto& ganster : g) {
-		if (visited.count(ganster.first) == 0) {
-			unordered_set<string> vlist;
-			dfs(vlist, ganster.first, g);
-			if (vlist.size() >= 3) {
-				int totle = 0;
-				string head;
-				for (const string& name : vlist) {
-					visited.insert(name);
-					totle += g[name].time;
-					if (head.empty() || g[name].time > g[head].time) {
-						head = name;
-					}
-				}
-				if (totle - g[head].time > k) {
-					++t;
-					res[head] = vlist.size();
-				}
-			}
-		}
-	}
-	cout << t << endl;
-	if (t > 0) {
-		for (const auto& p : res) {
-			cout << p.first << ' ' << p.second << endl;
-		}
-	}
-}
+//int main() {
+//	string name1, name2;
+//	int n, k, t; cin >> n >> k;
+//	map<string, Ganster> g;
+//	map<string, int> res;
+//	for (int i = 0; i < n; ++i) {
+//		cin >> name1 >> name2 >> t;
+//		g[name1].related.insert(name2);
+//		g[name1].time += t;
+//
+//		g[name2].related.insert(name1);
+//		g[name2].time += t;
+//	}
+//	t = 0;
+//	unordered_set<string> visited;
+//	for (const auto& ganster : g) {
+//		if (visited.count(ganster.first) == 0) {
+//			unordered_set<string> vlist;
+//			dfs(vlist, ganster.first, g);
+//			if (vlist.size() >= 3) {
+//				int totle = 0;
+//				string head;
+//				for (const string& name : vlist) {
+//					visited.insert(name);
+//					totle += g[name].time;
+//					if (head.empty() || g[name].time > g[head].time) {
+//						head = name;
+//					}
+//				}
+//				if (totle - g[head].time > k) {
+//					++t;
+//					res[head] = vlist.size();
+//				}
+//			}
+//		}
+//	}
+//	cout << t << endl;
+//	if (t > 0) {
+//		for (const auto& p : res) {
+//			cout << p.first << ' ' << p.second << endl;
+//		}
+//	}
+//}
