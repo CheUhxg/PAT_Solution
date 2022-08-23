4080225d7fcb3fbfca07c9772dace39b230fc09c
cheuhxg <3193079152@qq.com>
2022-08-23 12:38:58 +0800

    finish

--- 1034.cpp
+++ 1034.cpp
@@ -8,56 +8,58 @@ using namespace std;
 
 struct Ganster {
 	unordered_set<string> related;
-	string head;
 	int time;
-	bool visited = false;
 };
 
-string FindHead(const string& me, map<string, Ganster>& record) {
-	if (!record[me].visited) {
-		record[me].visited = true;
-		string head(record[me].head);
-		for (const string& other : record[me].related) {
-			if (record[other].time > record[head].time) {
-				head = other;
-			}
+void dfs(unordered_set<string>& visited, const string& now, map<string, Ganster>& g) {
+	visited.insert(now);
+	for (const auto& other : g[now].related) {
+		if (visited.count(other) == 0) {
+			dfs(visited, other, g);
 		}
-		if (head != record[me].head)record[me].head = FindHead(head, record);
 	}
-	return record[me].head;
 }
 
 int main() {
 	string name1, name2;
 	int n, k, t; cin >> n >> k;
-	map<string, Ganster> record;
+	map<string, Ganster> g;
 	map<string, int> res;
 	for (int i = 0; i < n; ++i) {
 		cin >> name1 >> name2 >> t;
-		record[name1].related.insert(name2);
-		record[name1].head = name1;
-		record[name1].time += t;
+		g[name1].related.insert(name2);
+		g[name1].time += t;
 
-		record[name2].related.insert(name1);
-		record[name2].head = name2;
-		record[name2].time += t;
-	}
-	for (auto& ganster : record) {
-		ganster.second.head = FindHead(ganster.first, record);
-		res[ganster.second.head] += 10000;
-		if (ganster.second.head != ganster.first)
-			res[ganster.second.head] += record[ganster.first].time;
+		g[name2].related.insert(name1);
+		g[name2].time += t;
 	}
 	t = 0;
-	for (const auto& p : res) {
-		if (p.second / 10000 >= 3 && p.second % 10000 > k) ++t;
+	unordered_set<string> visited;
+	for (const auto& ganster : g) {
+		if (visited.count(ganster.first) == 0) {
+			unordered_set<string> vlist;
+			dfs(vlist, ganster.first, g);
+			if (vlist.size() >= 3) {
+				int totle = 0;
+				string head;
+				for (const string& name : vlist) {
+					visited.insert(name);
+					totle += g[name].time;
+					if (head.empty() || g[name].time > g[head].time) {
+						head = name;
+					}
+				}
+				if (totle - g[head].time > k) {
+					++t;
+					res[head] = vlist.size();
+				}
+			}
+		}
 	}
 	cout << t << endl;
 	if (t > 0) {
 		for (const auto& p : res) {
-			if (p.second / 10000 >= 3 && p.second % 10000 > k) {
-				cout << p.first << ' ' << p.second / 10000 << endl;
-			}
+			cout << p.first << ' ' << p.second << endl;
 		}
 	}
 }
