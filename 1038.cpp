31a23b89d5fe8052e2eeb836668e5fd1dd700044
cheuhxg <3193079152@qq.com>
2022-08-24 13:19:33 +0800

    finish 1038

--- 1038.cpp
+++ 1038.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
-int main() {
+bool cmp(const string& num1, const string& num2) {
+	int index = 0;
+	while (index < num1.size() && index < num2.size()) {
+		if (num1[index] != num2[index])return num1[index] < num2[index];
+		++index;
+	}
+	if (index < num1.size()) return cmp(num1.substr(index), num2);
+	if (index < num2.size()) return cmp(num1, num2.substr(index));
+	return true;
+}
 
+int main() {
+	int n; cin >> n;
+	string res;
+	vector<string> nums(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> nums[i];
+	}
+	sort(nums.begin(), nums.end(), cmp);
+	for (const string& num : nums) {
+		res += num;
+	}
+	n = res.find_first_not_of('0');
+	if (n == -1)cout << 0 << endl;
+	else cout << res.substr(n) << endl;
 }
