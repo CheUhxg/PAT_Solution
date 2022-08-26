514b6f39a5d9a8d5ef7e04541ae8701ccbc7cf88
cheuhxg <3193079152@qq.com>
2022-08-26 08:26:09 +0800

    mute

--- 1039.cpp
+++ 1039.cpp
@@ -6,24 +6,24 @@
 
 using namespace std;
 
-int main() {
-	int n, k, id, m; cin >> n >> k;
-	string name;
-	unordered_map<string, vector<int>> lists;
-	for (int i = 0; i < k; ++i) {
-		cin >> id >> m;
-		while (m--) {
-			cin >> name;
-			lists[name].push_back(id);
-		}
-	}
-	while (n--) {
-		cin >> name;
-		sort(lists[name].begin(), lists[name].end());
-		cout << name << ' ' << lists[name].size();
-		for (const int course : lists[name]) {
-			cout << ' ' << course;
-		}
-		cout << endl;
-	}
-}
+//int main() {
+//	int n, k, id, m; cin >> n >> k;
+//	string name;
+//	unordered_map<string, vector<int>> lists;
+//	for (int i = 0; i < k; ++i) {
+//		cin >> id >> m;
+//		while (m--) {
+//			cin >> name;
+//			lists[name].push_back(id);
+//		}
+//	}
+//	while (n--) {
+//		cin >> name;
+//		sort(lists[name].begin(), lists[name].end());
+//		cout << name << ' ' << lists[name].size();
+//		for (const int course : lists[name]) {
+//			cout << ' ' << course;
+//		}
+//		cout << endl;
+//	}
+//}
