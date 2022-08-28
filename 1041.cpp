9c025a6ab493fbf3edebd55f6a119f2c720c1001
cheuhxg <3193079152@qq.com>
2022-08-28 09:04:44 +0800

    mute

--- 1041.cpp
+++ 1041.cpp
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-int main() {
-	int n; cin >> n;
-	vector<int> nums(n);
-	unordered_map<int, int> visited;
-	for (int i = 0; i < n; ++i) {
-		cin >> nums[i];
-		++visited[nums[i]];
-	}
-	n = -1;
-	for (const int num : nums) {
-		if (visited[num] == 1) {
-			n = num;
-			break;
-		}
-	}
-	if (n == -1) cout << "None" << endl;
-	else cout << n << endl;
-}
+//int main() {
+//	int n; cin >> n;
+//	vector<int> nums(n);
+//	unordered_map<int, int> visited;
+//	for (int i = 0; i < n; ++i) {
+//		cin >> nums[i];
+//		++visited[nums[i]];
+//	}
+//	n = -1;
+//	for (const int num : nums) {
+//		if (visited[num] == 1) {
+//			n = num;
+//			break;
+//		}
+//	}
+//	if (n == -1) cout << "None" << endl;
+//	else cout << n << endl;
+//}
