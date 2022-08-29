0fcbe05beef3d565b963d922161948dc4ceab57b
cheuhxg <3193079152@qq.com>
2022-08-29 08:19:18 +0800

    mute

--- 1042.cpp
+++ 1042.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-char getCard(int& num) {
+static char getCard(int& num) {
 	int tmp(num);
 	num = (num - 1) % 13 + 1;
 	switch ((tmp - 1) / 13) {
@@ -22,26 +22,26 @@ char getCard(int& num) {
 	}
 }
 
-int main() {
-	int position[55] = { 0 }, res[55] = { 0 }, tmp[55] = { 0 };
-	int k, n; cin >> k;
-	for (int i = 1; i <= 54; ++i) {
-		cin >> n;
-		position[n] = i;
-		res[n] = i;
-	}
-	int* prev = res, * next = tmp, * p;
-	while (--k) {
-		for (int i = 1; i <= 54; ++i) {
-			next[i] = prev[position[i]];
-		}
-		p = next;
-		next = prev;
-		prev = p;
-	}
-	cout << getCard(prev[1]) << prev[1];
-	for (int i = 2; i <= 54; ++i) {
-		cout << ' ' << getCard(prev[i]) << prev[i];
-	}
-	cout << endl;
-}
+//int main() {
+//	int position[55] = { 0 }, res[55] = { 0 }, tmp[55] = { 0 };
+//	int k, n; cin >> k;
+//	for (int i = 1; i <= 54; ++i) {
+//		cin >> n;
+//		position[n] = i;
+//		res[n] = i;
+//	}
+//	int* prev = res, * next = tmp, * p;
+//	while (--k) {
+//		for (int i = 1; i <= 54; ++i) {
+//			next[i] = prev[position[i]];
+//		}
+//		p = next;
+//		next = prev;
+//		prev = p;
+//	}
+//	cout << getCard(prev[1]) << prev[1];
+//	for (int i = 2; i <= 54; ++i) {
+//		cout << ' ' << getCard(prev[i]) << prev[i];
+//	}
+//	cout << endl;
+//}
