175da652824ae3c5939a806d68d5520722a4e251
cheuhxg <3193079152@qq.com>
2022-09-01 09:13:37 +0800

    failed on test point 3: time limit

---
+++ 1047.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+vector<string> student;
+
+bool cmp(const int a, const int b) {
+	return student[a] < student[b];
+}
+
+int main() {
+	int n, k, m, c; cin >> n >> k;
+	vector<vector<int>> course(k + 1, vector<int>());
+	student.resize(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> student[i] >> m;
+		while (m--) {
+			cin >> c;
+			course[c].push_back(i);
+		}
+	}
+	for (int i = 1; i <= k; ++i) {
+		sort(course[i].begin(), course[i].end(), cmp);
+		cout << i << ' ' << course[i].size() << endl;
+		for (const int& id : course[i]) {
+			cout << student[id] << endl;
+		}
+	}
+}
