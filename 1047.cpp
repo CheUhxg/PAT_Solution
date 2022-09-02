0b72ec779780923e8a9e8adb0dfc794b2624490e
cheuhxg <3193079152@qq.com>
2022-09-02 09:02:18 +0800

    mute

--- 1047.cpp
+++ 1047.cpp
@@ -5,28 +5,28 @@
 
 using namespace std;
 
-vector<string> student;
+static vector<string> student;
 
-bool cmp(const int a, const int b) {
+static bool cmp(const int a, const int b) {
 	return student[a] < student[b];
 }
 
-int main() {
-	int n, k, m, c; cin >> n >> k;
-	vector<vector<int>> course(k + 1, vector<int>());
-	student.resize(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> student[i] >> m;
-		while (m--) {
-			cin >> c;
-			course[c].push_back(i);
-		}
-	}
-	for (int i = 1; i <= k; ++i) {
-		sort(course[i].begin(), course[i].end(), cmp);
-		cout << i << ' ' << course[i].size() << endl;
-		for (const int& id : course[i]) {
-			printf("%s\n", student[id].c_str());
-		}
-	}
-}
+//int main() {
+//	int n, k, m, c; cin >> n >> k;
+//	vector<vector<int>> course(k + 1, vector<int>());
+//	student.resize(n);
+//	for (int i = 0; i < n; ++i) {
+//		cin >> student[i] >> m;
+//		while (m--) {
+//			cin >> c;
+//			course[c].push_back(i);
+//		}
+//	}
+//	for (int i = 1; i <= k; ++i) {
+//		sort(course[i].begin(), course[i].end(), cmp);
+//		cout << i << ' ' << course[i].size() << endl;
+//		for (const int& id : course[i]) {
+//			printf("%s\n", student[id].c_str());
+//		}
+//	}
+//}
