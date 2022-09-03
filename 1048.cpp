27fc618c7963fa3eae0008d679d3b7f6259f0ee8
cheuhxg <3193079152@qq.com>
2022-09-03 08:21:53 +0800

    mute

--- 1048.cpp
+++ 1048.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-int cnt[505];
+static int cnt[505];
 
-int main() {
-	int n, m, value, a = 0, b = 0; cin >> n >> m;
-	while (n--) {
-		cin >> value;
-		++cnt[value];
-	}
-	for (int i = 1; i * 2 <= m; ++i) {
-		if (m - i <= 500 && cnt[i] > 0 && cnt[m - i] > 0) {
-			if (i * 2 == m && cnt[i] > 1 || i * 2 < m) {
-				a = i;
-				b = m - i;
-				break;
-			}
-		}
-	}
-	if (a > 0)cout << a << ' ' << b << endl;
-	else cout << "No Solution" << endl;
-}
+//int main() {
+//	int n, m, value, a = 0, b = 0; cin >> n >> m;
+//	while (n--) {
+//		cin >> value;
+//		++cnt[value];
+//	}
+//	for (int i = 1; i * 2 <= m; ++i) {
+//		if (m - i <= 500 && cnt[i] > 0 && cnt[m - i] > 0) {
+//			if (i * 2 == m && cnt[i] > 1 || i * 2 < m) {
+//				a = i;
+//				b = m - i;
+//				break;
+//			}
+//		}
+//	}
+//	if (a > 0)cout << a << ' ' << b << endl;
+//	else cout << "No Solution" << endl;
+//}
