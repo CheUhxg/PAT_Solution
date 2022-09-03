7f4138e4598a5d07f13582c6b7bbac248af07b5c
cheuhxg <3193079152@qq.com>
2022-09-03 09:13:26 +0800

    finish 1049

---
+++ 1049.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int findOne(int n) {
+	int high, low = 0, mid, res = 0, base = 1;
+	while (n / base > 0) {
+		high = n / base / 10;
+		low = n % base;
+		mid = n / base % 10;
+		if (mid == 0)res += high * base;
+		else if (mid == 1)res += high * base + low + 1;
+		else res += (high + 1) * base;
+		base *= 10;
+	}
+	return res;
+}
+
+int main() {
+	int n; cin >> n;
+	cout << findOne(n) << endl;
+}
