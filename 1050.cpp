4b0934cd12fcdcb449c44ab141a458749a399709
cheuhxg <3193079152@qq.com>
2022-09-04 09:46:41 +0800

    finish 1050

---
+++ 1050.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	unordered_set<char> charInS2;
+	for (char c : s2) {
+		charInS2.insert(c);
+	}
+	int real = 0, index = 0;
+	while (index < s1.size()) {
+		if (charInS2.count(s1[index]) == 0) {
+			s1[real++] = s1[index];
+		}
+		++index;
+	}
+	s1.resize(real);
+	cout << s1 << endl;
+}
