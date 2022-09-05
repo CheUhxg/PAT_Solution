9d57fa69cf9328ac1d86f5e9aff2e82092083226
cheuhxg <3193079152@qq.com>
2022-09-05 08:12:36 +0800

    mute

--- 1050.cpp
+++ 1050.cpp
@@ -4,21 +4,21 @@
 
 using namespace std;
 
-int main() {
-	string s1, s2;
-	getline(cin, s1);
-	getline(cin, s2);
-	unordered_set<char> charInS2;
-	for (char c : s2) {
-		charInS2.insert(c);
-	}
-	int real = 0, index = 0;
-	while (index < s1.size()) {
-		if (charInS2.count(s1[index]) == 0) {
-			s1[real++] = s1[index];
-		}
-		++index;
-	}
-	s1.resize(real);
-	cout << s1 << endl;
-}
+//int main() {
+//	string s1, s2;
+//	getline(cin, s1);
+//	getline(cin, s2);
+//	unordered_set<char> charInS2;
+//	for (char c : s2) {
+//		charInS2.insert(c);
+//	}
+//	int real = 0, index = 0;
+//	while (index < s1.size()) {
+//		if (charInS2.count(s1[index]) == 0) {
+//			s1[real++] = s1[index];
+//		}
+//		++index;
+//	}
+//	s1.resize(real);
+//	cout << s1 << endl;
+//}
