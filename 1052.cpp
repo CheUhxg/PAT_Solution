f76258bfa273f9f70ca4b36186429d3810cd4eff
cheuhxg <3193079152@qq.com>
2022-09-06 09:46:00 +0800

    finish 1052

--- 1052.cpp
+++ 1052.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 struct Node {
-	string addr;
+	string addr, next;
 	int value;
+	bool isIn = false;
 };
 
 bool cmp(const Node& n1, const Node& n2) {
+	if (n1.isIn != n2.isIn)return n1.isIn > n2.isIn;
 	return n1.value < n2.value;
 }
 
 int main() {
-	int n, tmp; cin >> n >> tmp;
+	string start;
+	int n, cnt = 0; cin >> n >> start;
 	vector<Node> nodes(n + 1);
+	unordered_map<string, int> index;
 	nodes.back().addr = "-1";
 	nodes.back().value = 100005;
+	nodes.back().isIn = true;
+	index["-1"] = n;
 	for (int i = 0; i < n; ++i) {
-		cin >> nodes[i].addr >> nodes[i].value >> tmp;
+		cin >> nodes[i].addr >> nodes[i].value >> nodes[i].next;
+		index[nodes[i].addr] = i;
+	}
+	for (int i = index[start]; nodes[i].addr != "-1"; i = index[nodes[i].next]) {
+		nodes[i].isIn = true;
+		++cnt;
 	}
 	sort(nodes.begin(), nodes.end(), cmp);
-	cout << n << ' ' << nodes[0].addr << endl;
-	for (int i = 0; i < n; ++i) {
+	cout << cnt << ' ' << nodes[0].addr << endl;
+	for (int i = 0; i < cnt; ++i) {
 		cout << nodes[i].addr << ' '
 			<< nodes[i].value << ' '
 			<< nodes[i + 1].addr << endl;
