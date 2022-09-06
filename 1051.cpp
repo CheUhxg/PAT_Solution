6d1ec32603480cf159ed3d1a14d276f2e53fd9c1
cheuhxg <3193079152@qq.com>
2022-09-06 09:00:15 +0800

    mute

--- 1051.cpp
+++ 1051.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-int main() {
-	int m, n, k; cin >> m >> n >> k;
-	vector<int>nums(n);
-	while (k--) {
-		stack<int> stk;
-		for (int i = 0; i < n; ++i) {
-			cin >> nums[i];
-		}
-		int index = 0;
-		for (int i = 1; i <= n && stk.size() < m;++i) {
-			stk.push(i);
-			while (!stk.empty() && stk.top() == nums[index]) {
-				stk.pop();
-				++index;
-			}
-		}
-		if (index == n)cout << "YES" << endl;
-		else cout << "NO" << endl;
-	}
-}
+//int main() {
+//	int m, n, k; cin >> m >> n >> k;
+//	vector<int>nums(n);
+//	while (k--) {
+//		stack<int> stk;
+//		for (int i = 0; i < n; ++i) {
+//			cin >> nums[i];
+//		}
+//		int index = 0;
+//		for (int i = 1; i <= n && stk.size() < m;++i) {
+//			stk.push(i);
+//			while (!stk.empty() && stk.top() == nums[index]) {
+//				stk.pop();
+//				++index;
+//			}
+//		}
+//		if (index == n)cout << "YES" << endl;
+//		else cout << "NO" << endl;
+//	}
+//}
