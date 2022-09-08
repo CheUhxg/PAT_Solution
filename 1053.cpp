d0582fa7ca6122ba9d2b245224c12c34d936f766
cheuhxg <3193079152@qq.com>
2022-09-08 10:19:39 +0800

    mute

--- 1053.cpp
+++ 1053.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-struct Node {
+static struct Node {
 	int w;
 	vector<int> sons;
 };
 
-vector<vector<int>> res;
+static vector<vector<int>> res;
 
-void dfs(const int index, const int weight, const vector<Node>& tree, vector<int>& path) {
+static void dfs(const int index, const int weight, const vector<Node>& tree, vector<int>& path) {
 	if (tree[index].sons.size() == 0) {
 		if (tree[index].w == weight) {
 			res.push_back(path);
@@ -30,34 +30,34 @@ void dfs(const int index, const int weight, const vector<Node>& tree, vector<int
 	}
 }
 
-bool cmp(const vector<int>& v1, const vector<int>& v2) {
+static bool cmp(const vector<int>& v1, const vector<int>& v2) {
 	return v1 > v2;
 }
 
-int main() {
-	int n, m, s, id, num, root = 0; cin >> n >> m >> s;
-	vector<Node> tree(n);
-	vector<bool> visited(n, false);
-	for (int i = 0; i < n; ++i) {
-		cin >> tree[i].w;
-	}
-	for (int i = 0; i < m; ++i) {
-		cin >> id >> num;
-		tree[id].sons.resize(num);
-		for (int j = 0; j < num; ++j) {
-			cin >> tree[id].sons[j];
-			visited[tree[id].sons[j]] = true;
-		}
-	}
-	while (visited[root])++root;
-	vector<int> path;
-	dfs(root, s, tree, path);
-	sort(res.begin(), res.end(), cmp);
-	for (const vector<int>& v : res) {
-		cout << tree[root].w;
-		for (int i = 0; i < v.size(); ++i) {
-			cout << ' ' << v[i];
-		}
-		cout << endl;
-	}
-}
+//int main() {
+//	int n, m, s, id, num, root = 0; cin >> n >> m >> s;
+//	vector<Node> tree(n);
+//	vector<bool> visited(n, false);
+//	for (int i = 0; i < n; ++i) {
+//		cin >> tree[i].w;
+//	}
+//	for (int i = 0; i < m; ++i) {
+//		cin >> id >> num;
+//		tree[id].sons.resize(num);
+//		for (int j = 0; j < num; ++j) {
+//			cin >> tree[id].sons[j];
+//			visited[tree[id].sons[j]] = true;
+//		}
+//	}
+//	while (visited[root])++root;
+//	vector<int> path;
+//	dfs(root, s, tree, path);
+//	sort(res.begin(), res.end(), cmp);
+//	for (const vector<int>& v : res) {
+//		cout << tree[root].w;
+//		for (int i = 0; i < v.size(); ++i) {
+//			cout << ' ' << v[i];
+//		}
+//		cout << endl;
+//	}
+//}
