bff91ee07d6575808ce95791b9d8f6c10c87b7a1
cheuhxg <3193079152@qq.com>
2022-09-09 09:53:07 +0800

    mute

--- 1054.cpp
+++ 1054.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-int main() {
-	int m, n, pix, now = -1, cnt = 0; cin >> m >> n;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			cin >> pix;
-			if (now == -1) {
-				now = pix;
-				++cnt;
-			}
-			else {
-				if (pix != now) {
-					if (--cnt == 0) {
-						now = pix;
-						++cnt;
-					}
-				}
-				else {
-					++cnt;
-				}
-			}
-		}
-	}
-	cout << now << endl;
-}
+//int main() {
+//	int m, n, pix, now = -1, cnt = 0; cin >> m >> n;
+//	for (int i = 0; i < n; ++i) {
+//		for (int j = 0; j < m; ++j) {
+//			cin >> pix;
+//			if (now == -1) {
+//				now = pix;
+//				++cnt;
+//			}
+//			else {
+//				if (pix != now) {
+//					if (--cnt == 0) {
+//						now = pix;
+//						++cnt;
+//					}
+//				}
+//				else {
+//					++cnt;
+//				}
+//			}
+//		}
+//	}
+//	cout << now << endl;
+//}
