762eb41a4ac9d633c4a7e36e0996db466a5a9003
cheuhxg <3193079152@qq.com>
2022-09-10 11:03:16 +0800

    finish 1056

---
+++ 1056.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Mouse {
+	int weight, round;
+};
+
+int main() {
+	int Np, Ng, index; cin >> Np >> Ng;
+	vector<Mouse>mouses(Np);
+	for (int i = 0; i < Np; ++i) {
+		cin >> mouses[i].weight;
+	}
+	queue<int>order;
+	for (int i = 0; i < Np; ++i) {
+		cin >> index;
+		order.push(index);
+	}
+	int exist = Np;
+	while (order.size() > 1) {
+		int group = (exist + Ng - 1) / Ng;
+		for (int r = 0; r < group; ++r) {
+			int winner = order.front();
+			for (int i = 0; i < Ng && r * Ng + i < exist; ++i) {
+				int other = order.front();
+				if (mouses[winner].weight < mouses[other].weight)winner = other;
+				mouses[other].round = group + 1;
+				order.pop();
+			}
+			order.push(winner);
+		}
+		exist = group;
+	}
+	mouses[order.front()].round = 1;
+	cout << mouses[0].round;
+	for (int i = 1; i < Np; ++i) {
+		cout << ' ' << mouses[i].round;
+	}
+	cout << endl;
+}
