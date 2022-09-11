e970fca3923765a88db8227c3c7c2b92ad38cd2c
CheUhxg <3193079152@qq.com>
2022-09-11 10:01:11 +0800

    finish 1058

---
+++ 1058.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int A[3] = { 0 }, B[3] = { 0 };
+	scanf("%d.%d.%d %d.%d.%d", &A[0], &A[1], &A[2], &B[0], &B[1], &B[2]);
+	if ((A[2] += B[2]) >= 29) {
+		A[2] %= 29;
+		++B[1];
+	}
+	if ((A[1] += B[1]) >= 17) {
+		A[1] %= 17;
+		++B[0];
+	}
+	A[0] += B[0];
+	printf("%d.%d.%d\n", A[0], A[1], A[2]);
+}
