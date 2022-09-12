b6896279c9be61b1b0f518ec1ee58a8e731dddb3
CheUhxg <3193079152@qq.com>
2022-09-12 10:20:02 +0800

    finish 1059

--- 1059.cpp
+++ 1059.cpp
@@ -7,6 +7,10 @@ int main() {
 	long int n, tmp; cin >> n;
 	map<int, int> cnt;
 	cout << n << '=';
+	if (n == 1) {
+		cout << 1 << endl;
+		return 0;
+	}
 	for (int i = 2; n > 1; ++i) {
 		while (n % i == 0) {
 			++cnt[i];
