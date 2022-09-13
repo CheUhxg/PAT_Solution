b16be0d4a921cb07b83f210f59441e7eccb621e3
CheUhxg <3193079152@qq.com>
2022-09-13 08:46:39 +0800

    mute

--- 1059.cpp
+++ 1059.cpp
@@ -3,30 +3,30 @@
 
 using namespace std;
 
-int main() {
-	long int n, tmp; cin >> n;
-	map<int, int> cnt;
-	cout << n << '=';
-	if (n == 1) {
-		cout << 1 << endl;
-		return 0;
-	}
-	for (int i = 2; n > 1; ++i) {
-		while (n % i == 0) {
-			++cnt[i];
-			n /= i;
-		}
-	}
-	for (auto iter = cnt.begin();
-		iter != cnt.end();
-		) {
-		cout << iter->first;
-		if (iter->second > 1) {
-			cout << '^' << iter->second;
-		}
-		if (++iter != cnt.end()) {
-			cout << '*';
-		}
-	}
-	cout << endl;
-}
+//int main() {
+//	long int n, tmp; cin >> n;
+//	map<int, int> cnt;
+//	cout << n << '=';
+//	if (n == 1) {
+//		cout << 1 << endl;
+//		return 0;
+//	}
+//	for (int i = 2; n > 1; ++i) {
+//		while (n % i == 0) {
+//			++cnt[i];
+//			n /= i;
+//		}
+//	}
+//	for (auto iter = cnt.begin();
+//		iter != cnt.end();
+//		) {
+//		cout << iter->first;
+//		if (iter->second > 1) {
+//			cout << '^' << iter->second;
+//		}
+//		if (++iter != cnt.end()) {
+//			cout << '*';
+//		}
+//	}
+//	cout << endl;
+//}
