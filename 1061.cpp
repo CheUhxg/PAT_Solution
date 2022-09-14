d714952fffdc242023ceb629611df4518e40135a
CheUhxg <3193079152@qq.com>
2022-09-14 10:11:33 +0800

    finish 1061

---
+++ 1061.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string week[7] = { "MON ", "TUE ", "WED ", "THU ", "FRI ", "SAT ", "SUN " };
+	char cap, chr;
+	string s1, s2, s3, s4;
+	getline(cin, s1);
+	getline(cin, s2);
+	getline(cin, s3);
+	getline(cin, s4);
+
+	int i = 0;
+	for (; ; ++i) {
+		if (s1[i] == s2[i]) {
+			if (s1[i] >= 'A' && s1[i] <= 'G') {
+				cap = s1[i];
+				break;
+			}
+		}
+	}
+	++i;
+	for (; i < s1.size(); ++i) {
+		if (s1[i] == s2[i]) {
+			if (s1[i] >= 'A' && s1[i] <= 'N' || isdigit(s1[i])) {
+				chr = s1[i];
+				break;
+			}
+		}
+	}
+	i = 0;
+	for (; i < s3.size(); ++i) {
+		if (s3[i] == s4[i] && isalpha(s3[i])) {
+			break;
+		}
+	}
+	cout << week[cap - 'A'];
+	printf("%02d:%02d\n", isdigit(chr) ? chr - '0' : chr - 'A' + 10, i);
+}
