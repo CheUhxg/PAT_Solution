94fc7263926d91f90a45b5961b86ea02b639db06
CheUhxg <3193079152@qq.com>
2022-09-18 09:55:09 +0800

    finish 1065

--- 1065.cpp
+++ 1065.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
 	int t; cin >> t;
+	bool res;
 	long long a, b, c, sum;
 	for (int i = 1; i <= t; ++i) {
-		cin >> a >> b >> c;
+		scanf("%lld %lld %lld", &a, &b, &c);
 		sum = a + b;
-		if (sum < 0) {
-			if (a > 0 && b > 0)
-				cout << "Case #" << i << ": " << "true";
-			else
-				cout << "Case #" << i << ": " << (sum > c ? "true" : "false");
-		}
-		else {
-			if (a < 0 && b < 0)
-				cout << "Case #" << i << ": " << "false";
-			else
-				cout << "Case #" << i << ": " << (sum > c ? "true" : "false");
-		}
+		if (a > 0 && b > 0 && sum < 0) res = true;
+		else if (a < 0 && b < 0 && sum >= 0) res = false;
+		else if (sum > c) res = true;
+		else res = false;
+
+		if(res)
+			cout << "Case #" << i << ": " << "true";
+		else
+			cout << "Case #" << i << ": " << "false";
 		if (i != t)cout << endl;
 	}
 }
