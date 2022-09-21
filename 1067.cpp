d989abeefd81f01307e305608ef77c8d0f19b73b
CheUhxg <3193079152@qq.com>
2022-09-21 08:23:02 +0800

    mute

--- 1067.cpp
+++ 1067.cpp
@@ -4,25 +4,25 @@
 
 using namespace std;
 
-int main() {
-	int n, cnt; cin >> n;
-	vector<int> n2i(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> cnt;
-		n2i[cnt] = i;
-	}
-	cnt = 0;
-	for (int i = 1; i < n; ++i) {
-		while (i != n2i[i]) {
-			while (n2i[0] != 0) {
-				swap(n2i[0], n2i[n2i[0]]);
-				++cnt;
-			}
-			if (i != n2i[i]) {
-				swap(n2i[0], n2i[i]);
-				++cnt;
-			}
-		}
-	}
-	cout << cnt << endl;
-}
+//int main() {
+//	int n, cnt; cin >> n;
+//	vector<int> n2i(n);
+//	for (int i = 0; i < n; ++i) {
+//		cin >> cnt;
+//		n2i[cnt] = i;
+//	}
+//	cnt = 0;
+//	for (int i = 1; i < n; ++i) {
+//		while (i != n2i[i]) {
+//			while (n2i[0] != 0) {
+//				swap(n2i[0], n2i[n2i[0]]);
+//				++cnt;
+//			}
+//			if (i != n2i[i]) {
+//				swap(n2i[0], n2i[i]);
+//				++cnt;
+//			}
+//		}
+//	}
+//	cout << cnt << endl;
+//}
