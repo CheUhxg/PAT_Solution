4a4bb46184ce7146ce7ae0a536b2f270ecd1ef50
CheUhxg <3193079152@qq.com>
2022-09-28 08:26:28 +0800

    mute

--- 1074.cpp
+++ 1074.cpp
@@ -6,33 +6,33 @@
 
 using namespace std;
 
-struct Node {
+static struct Node {
 	int add, data, next;
 };
 
-int main() {
-	int start, n, k; cin >> start >> n >> k;
-	unordered_map<int, Node> nodes;
-	vector<Node>res;
-	Node tmp;
-	for (int i = 0; i < n; ++i) {
-		cin >> tmp.add >> tmp.data >> tmp.next;
-		nodes[tmp.add] = tmp;
-	}
-	while (start != -1) {
-		res.push_back(nodes[start]);
-		start = res.back().next;
-	}
-	int low = 0;
-	while (low + k <= res.size()) {
-		reverse(res.begin() + low, res.begin() + low + k);
-		low += k;
-	}
-	for (int i = 0; i < res.size(); ++i) {
-		printf("%05d %d ", res[i].add, res[i].data);
-		if(i < res.size() - 1)
-			printf("%05d\n", res[i + 1].add);
-		else
-			printf("-1\n");
-	}
-}
+// int main() {
+// 	int start, n, k; cin >> start >> n >> k;
+// 	unordered_map<int, Node> nodes;
+// 	vector<Node>res;
+// 	Node tmp;
+// 	for (int i = 0; i < n; ++i) {
+// 		cin >> tmp.add >> tmp.data >> tmp.next;
+// 		nodes[tmp.add] = tmp;
+// 	}
+// 	while (start != -1) {
+// 		res.push_back(nodes[start]);
+// 		start = res.back().next;
+// 	}
+// 	int low = 0;
+// 	while (low + k <= res.size()) {
+// 		reverse(res.begin() + low, res.begin() + low + k);
+// 		low += k;
+// 	}
+// 	for (int i = 0; i < res.size(); ++i) {
+// 		printf("%05d %d ", res[i].add, res[i].data);
+// 		if(i < res.size() - 1)
+// 			printf("%05d\n", res[i + 1].add);
+// 		else
+// 			printf("-1\n");
+// 	}
+// }
