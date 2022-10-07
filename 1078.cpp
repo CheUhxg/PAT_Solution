28b800424d7520a908b5233cd43d03f1a43506b4
CheUhxg <3193079152@qq.com>
2022-10-07 09:05:15 +0800

    mute

--- 1078.cpp
+++ 1078.cpp
@@ -3,36 +3,36 @@
 
 using namespace std;
 
-int main() {
-    int m, n, t = -1; cin >> m >> n;
-    if(m == 1)t = 2;
-    else {
-        vector<bool>isPrime(m, true);
-        for(int i = 1; i <= m; ++i) {
-            for(int j = i; j >= 2; --j) {
-                if(i * j >= m && i * j < 2 * m) {
-                    isPrime[i * j - m] = false;
-                }
-            }
-        }
-        while(!isPrime[++t]);
-        t += m;
-    }
-    vector<bool>visited(t, false);
-    while(n--) {
-        cin >> m;
-        int i;
-        for(i = 0; i < t; ++i) {
-            if(!visited[(m + i * i) % t]) {
-                break;
-            }
-        }
-        if(!visited[(m + i * i) % t]) {
-            visited[(m + i * i) % t] = true;
-            cout << (m + i * i) % t;
-        }
-        else cout << '-';
-        if(n > 0)cout << ' ';
-    }
-    cout << endl;
-}
+//int main() {
+//    int m, n, t = -1; cin >> m >> n;
+//    if(m == 1)t = 2;
+//    else {
+//        vector<bool>isPrime(m, true);
+//        for(int i = 1; i <= m; ++i) {
+//            for(int j = i; j >= 2; --j) {
+//                if(i * j >= m && i * j < 2 * m) {
+//                    isPrime[i * j - m] = false;
+//                }
+//            }
+//        }
+//        while(!isPrime[++t]);
+//        t += m;
+//    }
+//    vector<bool>visited(t, false);
+//    while(n--) {
+//        cin >> m;
+//        int i;
+//        for(i = 0; i < t; ++i) {
+//            if(!visited[(m + i * i) % t]) {
+//                break;
+//            }
+//        }
+//        if(!visited[(m + i * i) % t]) {
+//            visited[(m + i * i) % t] = true;
+//            cout << (m + i * i) % t;
+//        }
+//        else cout << '-';
+//        if(n > 0)cout << ' ';
+//    }
+//    cout << endl;
+//}
