5bb74bd402da7df7e245437dd03c0a8e395e2c6e
CheUhxg <3193079152@qq.com>
2022-10-08 15:08:55 +0800

    mute

--- 1079.cpp
+++ 1079.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-double sum = 0, r;
+static double sum = 0, r;
 
-void dfs(int index, double rate,
+static void dfs(int index, double rate,
          const vector<vector<int>>& tree) {
     if(tree[index][0] == 0) {
         sum += rate * tree[index][1];
@@ -17,25 +17,25 @@ void dfs(int index, double rate,
     }
 }
 
-int main() {
-    int n, t;
-    double p;
-    cin >> n >> p >> r;
-    r /= 100;
-    vector<vector<int>> tree(n, vector<int>());
-    for(int i = 0; i < n; ++i) {
-        cin >> t;
-        if(t > 0) {
-            tree[i].resize(t);
-            for(int j = 0; j < t; ++j) {
-                cin >> tree[i][j];
-            }
-        } else {
-            tree[i].resize(2);
-            tree[i][0] = 0;
-            cin >> tree[i][1];
-        }
-    }
-    dfs(0, 1, tree);
-    printf("%.1f\n", sum * p);
-}
+//int main() {
+//    int n, t;
+//    double p;
+//    cin >> n >> p >> r;
+//    r /= 100;
+//    vector<vector<int>> tree(n, vector<int>());
+//    for(int i = 0; i < n; ++i) {
+//        cin >> t;
+//        if(t > 0) {
+//            tree[i].resize(t);
+//            for(int j = 0; j < t; ++j) {
+//                cin >> tree[i][j];
+//            }
+//        } else {
+//            tree[i].resize(2);
+//            tree[i][0] = 0;
+//            cin >> tree[i][1];
+//        }
+//    }
+//    dfs(0, 1, tree);
+//    printf("%.1f\n", sum * p);
+//}
