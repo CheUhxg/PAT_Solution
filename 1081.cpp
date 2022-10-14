84f7a8701448eb5f4cc5abd968c4bfeaf9b50fc3
CheUhxg <3193079152@qq.com>
2022-10-14 21:12:35 +0800

    finish 1081

--- 1081.cpp
+++ 1081.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 struct Rational {
-    long int numerator, denominator;
+    long int numerator = 0, denominator = 1;
 };
 
 long int gcd(long int a, long int b) {
@@ -18,24 +17,28 @@ long int lcm(long int a, long int b) {
 
 int main() {
     int n; cin >> n;
-    long int d = 1, sum = 0, integer;
+    Rational sum;
     char c;
-    vector<Rational>rationals(n);
     for(int i = 0; i < n; ++i) {
-        cin >> rationals[i].numerator >> c >> rationals[i].denominator;
-        d = lcm(d, rationals[i].denominator);
+        Rational now;
+        cin >> now.numerator >> c >> now.denominator;
+        long int d = lcm(sum.denominator, now.denominator);
+        sum.numerator *= d / sum.denominator;
+        sum.numerator += now.numerator * (d / now.denominator);
+        sum.denominator = d;
+        d = gcd(abs(sum.numerator), sum.denominator);
+        sum.numerator /= d;
+        sum.denominator /= d;
     }
-    for(const Rational& r : rationals) {
-        sum += r.numerator * (d / r.denominator);
+    long int integer = sum.numerator / sum.denominator;
+    sum.numerator %= sum.denominator;
+    if(integer != 0) {
+        cout << integer;
+        if(sum.numerator != 0) cout << ' ';
     }
-    integer = sum / d;
-    if(integer > 0) {
-        cout << integer << ' ';
-    }
-    sum %= d;
-    if(sum > 0) {
-        integer = gcd(sum, d);
-        cout << sum / integer << '/' << d / integer << endl;
+    if(sum.numerator != 0) {
+        integer = gcd(abs(sum.numerator + sum.denominator), sum.denominator);
+        cout << sum.numerator / integer << '/' << sum.denominator / integer << endl;
     }
     else if(integer == 0) {
         cout << 0 << endl;
