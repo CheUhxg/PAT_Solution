5425bc3c22d78088fd5b7cad5eaff22aaf2f906c
CheUhxg <3193079152@qq.com>
2022-11-07 18:15:32 +0800

    failed on test point 3

--- 1082.cpp
+++ 1082.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
 
 using namespace std;
 
@@ -12,30 +11,29 @@ const char* numberC[10] = {
     "wu", "liu", "qi", "ba", "jiu"
 };
 
-bool readInChinese(vector<string>& res, int& num) {
-    bool isNull[4];
+void readInChinese(vector<string>& res, int& num) {
+    bool isAdd = false;
     for(int base = 0; base < 4; ++base) {
-        isNull[base] = num % 10 == 0;
         if(num % 10) {
+            isAdd = true;
             if(base > 0)res.emplace_back(mountC[base]);
             res.emplace_back(numberC[num % 10]);
+        } else if(isAdd && num / 10 > 0) {
+            res.emplace_back("ling");
+            isAdd = false;
         }
         num /= 10;
     }
-    return isNull[3] ||
-           (isNull[2] && (!isNull[1] || !isNull[0])) ||
-           (!isNull[2] && isNull[1] && !isNull[0]);
 }
 
 int main() {
     int num; cin >> num;
     vector<string> res;
-    bool neg = num < 0, isZero;
+    bool neg = num < 0;
     num = abs(num);
     for(auto & base : levelC) {
-        if(num % 10000 > 0) isZero = readInChinese(res, num);
+        if(num % 10000 > 0) readInChinese(res, num);
         if(num > 0) {
-            if(isZero)res.emplace_back("ling");
             res.emplace_back(base);
         }
     }
