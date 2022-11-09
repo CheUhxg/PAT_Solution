22137597eaed9ec1c7853f773e5d78d48f08488f
CheUhxg <3193079152@qq.com>
2022-11-09 21:43:19 +0800

    mute

--- 1082.cpp
+++ 1082.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-const char* levelC[3] = {"Wan", "Yi"};
-const char* mountC[4] = {"", "Shi", "Bai", "Qian"};
-const char* numberC[10] = {
+static const char* levelC[3] = {"Wan", "Yi"};
+static const char* mountC[4] = {"", "Shi", "Bai", "Qian"};
+static const char* numberC[10] = {
     "ling", "yi", "er", "san", "si",
     "wu", "liu", "qi", "ba", "jiu"
 };
 
-void readInChinese(vector<string>& res, int& num) {
+static void readInChinese(vector<string>& res, int& num) {
     bool isAdd = false;
     for(int base = 0; base < 4; ++base) {
         if(num % 10) {
@@ -26,32 +26,32 @@ void readInChinese(vector<string>& res, int& num) {
     }
 }
 
-int main() {
-    int num; cin >> num;
-    if(num == 0) {
-        cout << "ling" << endl;
-        return 0;
-    }
-    vector<string> res;
-    bool neg = num < 0;
-    num = abs(num);
-    for(auto & base : levelC) {
-        readInChinese(res, num);
-        if(num > 0 && num % 10000 > 0) {
-            res.emplace_back(base);
-        } else if(num > 0) {
-            res.emplace_back("ling");
-        } else {
-            break;
-        }
-    }
-    reverse(res.begin(), res.end());
-    if(neg)cout << "Fu ";
-    cout << res[0];
-    for(int i = 1; i < res.size(); ++i) {
-        if(!res[i].empty()) {
-            cout << ' ' << res[i];
-        }
-    }
-    cout << endl;
-}
+//int main() {
+//    int num; cin >> num;
+//    if(num == 0) {
+//        cout << "ling" << endl;
+//        return 0;
+//    }
+//    vector<string> res;
+//    bool neg = num < 0;
+//    num = abs(num);
+//    for(auto & base : levelC) {
+//        readInChinese(res, num);
+//        if(num > 0 && num % 10000 > 0) {
+//            res.emplace_back(base);
+//        } else if(num > 0) {
+//            res.emplace_back("ling");
+//        } else {
+//            break;
+//        }
+//    }
+//    reverse(res.begin(), res.end());
+//    if(neg)cout << "Fu ";
+//    cout << res[0];
+//    for(int i = 1; i < res.size(); ++i) {
+//        if(!res[i].empty()) {
+//            cout << ' ' << res[i];
+//        }
+//    }
+//    cout << endl;
+//}
