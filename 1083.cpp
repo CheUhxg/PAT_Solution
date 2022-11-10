a641948a0436d41cd2247837c7a186bf4919a479
CheUhxg <3193079152@qq.com>
2022-11-10 14:17:19 +0800

    mute

--- 1083.cpp
+++ 1083.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-struct Record {
+static struct Record {
     string name, id;
     uint16_t score;
 };
 
-uint16_t low, high;
+static uint16_t low, high;
 
-bool cmp(const Record& r1, const Record& r2) {
+static bool cmp(const Record& r1, const Record& r2) {
     if(r1.score >= low && r1.score <= high &&
         r2.score >= low && r2.score <= high) {
         return r1.score > r2.score;
@@ -22,21 +22,21 @@ bool cmp(const Record& r1, const Record& r2) {
     }
 }
 
-int main() {
-    int n; cin >> n;
-    vector<Record>records(n);
-    for(auto & r : records) {
-        cin >> r.name >> r.id >> r.score;
-    }
-    cin >> low >> high;
-    sort(records.begin(), records.end(), cmp);
-    if(records[0].score < low || records[0].score > high) {
-        cout << "NONE" << endl;
-    } else {
-        for(auto & r : records) {
-            if(r.score < low || r.score > high)break;
-            cout << r.name << ' '
-                << r.id << endl;
-        }
-    }
-}
+//int main() {
+//    int n; cin >> n;
+//    vector<Record>records(n);
+//    for(auto & r : records) {
+//        cin >> r.name >> r.id >> r.score;
+//    }
+//    cin >> low >> high;
+//    sort(records.begin(), records.end(), cmp);
+//    if(records[0].score < low || records[0].score > high) {
+//        cout << "NONE" << endl;
+//    } else {
+//        for(auto & r : records) {
+//            if(r.score < low || r.score > high)break;
+//            cout << r.name << ' '
+//                << r.id << endl;
+//        }
+//    }
+//}
