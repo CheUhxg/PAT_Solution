e083d3032c9af4841570430fddaae4946654e4a4
CheUhxg <3193079152@qq.com>
2022-11-18 20:27:40 +0800

    mute

--- 1085.cpp
+++ 1085.cpp
@@ -4,27 +4,27 @@
 
 using namespace std;
 
-int main() {
-    long long p;
-    int n; cin >> n >> p;
-    vector<long long> num(n);
-    for(long long& i : num) {
-        cin >> i;
-    }
-    sort(num.begin(), num.end());
-    int low = 0, high = 0, max_len = 0;
-    while(high < num.size()) {
-        if(num[high] <= num[low] * p) {
-            ++high;
-        } else {
-            if(high - low > max_len) {
-                max_len = high - low;
-            }
-            ++low;
-        }
-    }
-    if(high - low > max_len) {
-        max_len = high - low;
-    }
-    cout << max_len << endl;
-}
+//int main() {
+//    long long p;
+//    int n; cin >> n >> p;
+//    vector<long long> num(n);
+//    for(long long& i : num) {
+//        cin >> i;
+//    }
+//    sort(num.begin(), num.end());
+//    int low = 0, high = 0, max_len = 0;
+//    while(high < num.size()) {
+//        if(num[high] <= num[low] * p) {
+//            ++high;
+//        } else {
+//            if(high - low > max_len) {
+//                max_len = high - low;
+//            }
+//            ++low;
+//        }
+//    }
+//    if(high - low > max_len) {
+//        max_len = high - low;
+//    }
+//    cout << max_len << endl;
+//}
