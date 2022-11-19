d9d027be8d603cc48acfadc3ad571dc6847b5445
CheUhxg <3193079152@qq.com>
2022-11-19 20:03:39 +0800

    mute

--- 1086.cpp
+++ 1086.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-struct Node {
+static struct Node {
     int left, right;
 };
 
-int n, tree_root, times;
-string pattern;
+static int n, tree_root, times;
+static string pattern;
 
-void dfs(int root, vector<Node>& tree) {
+static void dfs(int root, vector<Node>& tree) {
     int index;
 
     if(--times == 0)return;
@@ -30,19 +30,19 @@ void dfs(int root, vector<Node>& tree) {
     }
 }
 
-void show(int root, const vector<Node>& tree) {
+static void show(int root, const vector<Node>& tree) {
     if(tree[root].left > 0) show(tree[root].left, tree);
     if(tree[root].right > 0) show(tree[root].right, tree);
     cout << root << ' ';
 }
 
-int main() {
-    cin >> n;
-    times = 2 * n;
-    vector<Node> tree(n+1, {0, 0});
-    cin >> pattern >> tree_root; cin.get();
-    dfs(tree_root, tree);
-    if(tree[tree_root].left > 0) show(tree[tree_root].left, tree);
-    if(tree[tree_root].right > 0) show(tree[tree_root].right, tree);
-    cout << tree_root << endl;
-}
+//int main() {
+//    cin >> n;
+//    times = 2 * n;
+//    vector<Node> tree(n+1, {0, 0});
+//    cin >> pattern >> tree_root; cin.get();
+//    dfs(tree_root, tree);
+//    if(tree[tree_root].left > 0) show(tree[tree_root].left, tree);
+//    if(tree[tree_root].right > 0) show(tree[tree_root].right, tree);
+//    cout << tree_root << endl;
+//}
